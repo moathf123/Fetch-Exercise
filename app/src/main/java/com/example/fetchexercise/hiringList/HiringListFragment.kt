bbd3561a7ac27d4a718eeeeec02b646c176acddb
moath farhoud <moathf123@gmail.com>
2021-06-09 11:06:52 +0300

    implementing recycler view display

--- app/src/main/java/com/example/fetchexercise/hiringList/HiringListFragment.kt
+++ app/src/main/java/com/example/fetchexercise/hiringList/HiringListFragment.kt
@@ -4,27 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.fetchexercise.R
+import com.example.fetchexercise.databinding.FragmentHiringListBinding
 
 
 class HiringListFragment : Fragment() {
 
     private lateinit var viewModel: HiringListViewModel
+    private lateinit var binding: FragmentHiringListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hiring_list, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_hiring_list, container, false
+        )
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(HiringListViewModel::class.java)
 
+        val adapter = HiringAdapter()
+        binding.recyclerView.adapter = adapter
 
+        viewModel.hiring.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
     }
 }
--- app/src/main/java/com/example/fetchexercise/hiringList/HiringListViewModel.kt
+++ app/src/main/java/com/example/fetchexercise/hiringList/HiringListViewModel.kt
@@ -1,14 +1,20 @@
 package com.example.fetchexercise.hiringList
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fetchexercise.network.Network
+import com.example.fetchexercise.network.NetworkHiring
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class HiringListViewModel : ViewModel() {
+
+    private var _hirings = MutableLiveData<List<NetworkHiring>>()
+    val hiring: LiveData<List<NetworkHiring>>
+        get() = _hirings
 
     init {
         viewModelScope.launch {
@@ -17,10 +23,10 @@ class HiringListViewModel : ViewModel() {
     }
 
 
-    suspend fun fetchHiring() {
+    private suspend fun fetchHiring() {
         withContext(Dispatchers.IO) {
             val hiringList = Network.Hirings.getHiringlist().await()
-            Log.i("HiringListViewModel", hiringList.size.toString())
+            _hirings.postValue(hiringList)
         }
     }
 }
